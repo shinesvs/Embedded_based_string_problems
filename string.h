b1ef32fafc95cb3fa218f10b2520c8a51cc3d8b9
Shine Vattuparampil Sam <156725278+shinesvs@users.noreply.github.com>
2025-07-11 17:11:43 -0700

    Create string.h

---
+++ string.h
@@ -0,0 +1,9 @@
+#ifndef STRING__H
+#define STRING__H
+
+int str_len ( char * str );
+int my_count_digits( const char * str );
+char * remove_spaces( const char * str);
+int string_compare ( char * str1 , char * str2 );
+
+#endif STRING__H
